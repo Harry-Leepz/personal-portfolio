0c4fddadbefa79b79025fb6527caff6ad5716fda
Harry Dhillon <hdhillon478@gmail.com>
2024-11-16 16:12:40 +0000

    feat: add navigation links data for site structure

---
+++ lib/data.ts
@@ -0,0 +1,6 @@
+export const navLinks = [
+  { name: "Home", hash: "/" },
+  { name: "About", hash: "/about" },
+  { name: "Projects", hash: "/projects" },
+  { name: "Contact", hash: "/contact" },
+];
