e9541a6d6f7bae2cc2093b9f13c6758e99656410
Harry Dhillon <hdhillon478@gmail.com>
2024-11-17 10:55:48 +0000

    fix: reduce transition delay for intro component animations

--- components/sections/intro.tsx
+++ components/sections/intro.tsx
@@ -66,7 +66,7 @@ export default function Intro() {
         className='flex flex-col sm:flex-row justify-center gap-2 px-4 text-lg font-medium'
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.2 }}
+        transition={{ delay: 0.1 }}
       >
         <Link
           href='#contact'
