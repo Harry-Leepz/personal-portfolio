08e9bdc576ef702fa56accd3a61e8c0cee86cb6e
Harry Dhillon <hdhillon478@gmail.com>
2024-11-17 16:03:02 +0000

    chore: add project data with images and descriptions to enhance portfolio display

--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,7 @@
+import nandlImage from "@/public/nandl.png";
+import devjobsImage from "@/public/dev-jobs.png";
+import primehavenImage from "@/public/prime-haven.png";
+
 export const navLinks = [
   { name: "Home", hash: "#home" },
   { name: "About", hash: "#about" },
@@ -5,4 +9,36 @@ export const navLinks = [
   { name: "Skills", hash: "#skills" },
   { name: "Experience", hash: "#experience" },
   { name: "Contact", hash: "#contact" },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Nourish and Lift",
+    description:
+      "E-Commerce website selling basic starting gym equipment, like dubmbells, kettlebells, and body weights.",
+    tags: ["Python", "Django", "PostgresSQL", "Bootstrap", "Stripe"],
+    imageUrl: nandlImage,
+  },
+  {
+    title: "Prime Haven",
+    description:
+      "A rental property website, allowing users to search for properties, view details, and contact the owner.",
+    tags: [
+      "React",
+      "JavaScript",
+      "Next.js",
+      "Next-Auth",
+      "Cloudinary",
+      "Tailwind",
+      "MongoDB",
+    ],
+    imageUrl: primehavenImage,
+  },
+  {
+    title: "Developer Jobs",
+    description:
+      "Inspired by Indeed, this website allows users to search for developer jobs by technology.",
+    tags: ["React", "TypeScript", "Tanstack-React Query", "Radix UI", "CSS"],
+    imageUrl: devjobsImage,
+  },
 ] as const;
