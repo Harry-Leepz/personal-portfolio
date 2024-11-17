870b6cf828a740c17fcdee9983960bd9dcadd250
Harry Dhillon <hdhillon478@gmail.com>
2024-11-17 17:45:34 +0000

    feat: enable smooth scrolling by adding class to HTML element

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang='en' className='!scroll-smooth'>
       <body
         className={`${inter.className} bg-slate-200 text-slate-950 pt-28 sm:pt-36`}
       >
