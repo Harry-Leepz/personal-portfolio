ce4fca3b68588650a9948266c02111ad7f61f7bd
Harry Dhillon <hdhillon478@gmail.com>
2024-11-23 19:16:35 +0000

    feat: enhance Skills component to display a list of skills with SectionHeading

--- components/sections/skills.tsx
+++ components/sections/skills.tsx
@@ -1,3 +1,16 @@
+import SectionHeading from "./section-heading";
+
+import { skillsData } from "@/lib/data";
+
 export default function Skills() {
-  return <div>skills</div>;
+  return (
+    <section>
+      <SectionHeading>My Skills</SectionHeading>
+      <ul>
+        {skillsData.map((skill) => (
+          <li key={skill}>{skill}</li>
+        ))}
+      </ul>
+    </section>
+  );
 }
