0a888a4ddcd39f9e9e166a08ad7e163eae30f024
Harry Dhillon <hdhillon478@gmail.com>
2024-11-27 20:26:55 +0000

    feat: add Experience component

---
+++ components/sections/experience.tsx
@@ -0,0 +1,15 @@
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+
+import SectionHeading from "./section-heading";
+
+export default function Experience() {
+  return (
+    <section id='experience'>
+      <SectionHeading>My Experience</SectionHeading>
+    </section>
+  );
+}
