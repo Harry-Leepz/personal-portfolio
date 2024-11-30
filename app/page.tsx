07a7f25733a7f6e9e0d23c4d50995fb06e1500f2
Harry Dhillon <hdhillon478@gmail.com>
2024-11-30 13:40:42 +0000

    feat: add Contact component and include it in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/sections/about";
+import Contact from "@/components/sections/contact";
 import Experience from "@/components/sections/experience";
 import Intro from "@/components/sections/intro";
 import Projects from "@/components/sections/projects";
@@ -14,6 +15,7 @@ export default function Home() {
       <Projects />
       <Skills />
       <Experience />
+      <Contact />
     </main>
   );
 }
