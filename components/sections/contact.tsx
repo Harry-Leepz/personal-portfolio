a2795cffcdfc809f3a5b5047cb798c5cb662010b
Harry Dhillon <hdhillon478@gmail.com>
2024-11-30 15:09:45 +0000

    feat: enhance Contact component with form and email link

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -1,3 +1,31 @@
+import { FaPaperPlane } from "react-icons/fa";
+
+import SectionHeading from "./section-heading";
+
 export default function Contact() {
-  return <section>contact</section>;
+  return (
+    <section id='contact'>
+      <SectionHeading>Contact Me</SectionHeading>
+
+      <p>
+        Please contact me directly via{" "}
+        <a
+          className='italic underline font-semibold'
+          href='mailto:hdhillon478@gmail.com'
+          target='_blank'
+        >
+          email
+        </a>{" "}
+        or through this form
+      </p>
+
+      <form action=''>
+        <input type='email' />
+        <textarea name='' />
+        <button type='submit'>
+          Submit <FaPaperPlane />
+        </button>
+      </form>
+    </section>
+  );
 }
