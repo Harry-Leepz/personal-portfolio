5db3a339e03e4396154b176cba1f7a0150138e3c
Harry Dhillon <hdhillon478@gmail.com>
2024-11-30 15:36:25 +0000

    feat: enhance Contact section with animation and improved form styling

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -1,13 +1,27 @@
+"use client";
+
+import { motion } from "motion/react";
 import { FaPaperPlane } from "react-icons/fa";
 
+import useSectionInView from "@/lib/hooks";
 import SectionHeading from "./section-heading";
 
 export default function Contact() {
+  const { ref } = useSectionInView("Contact");
+
   return (
-    <section id='contact'>
+    <motion.section
+      id='contact'
+      className='mb-20 sm:mb-28 w-[min(100%, 38rem)] text-center'
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      ref={ref}
+      viewport={{ once: true }}
+    >
       <SectionHeading>Contact Me</SectionHeading>
 
-      <p>
+      <p className='text-slate-700 -mt-6'>
         Please contact me directly via{" "}
         <a
           className='italic underline font-semibold'
@@ -16,16 +30,27 @@ export default function Contact() {
         >
           email
         </a>{" "}
-        or through this form
+        or through this form.
       </p>
 
-      <form action=''>
-        <input type='email' />
-        <textarea name='' />
-        <button type='submit'>
-          Submit <FaPaperPlane />
+      <form action='' className='mt-10 flex flex-col'>
+        <input
+          type='email'
+          className='h-14 rounded-lg border border-black/20 px-4'
+          placeholder='Your email'
+        />
+        <textarea
+          placeholder="What's on your mind?"
+          className='h-52 rounded-lg border border-black/20 p-4 my-3'
+        />
+        <button
+          type='submit'
+          className='h-[3rem] w-[8rem] rounded-full outline-none transition-all text-white bg-slate-900 flex items-center justify-center gap-2 group focus:scale-110 hover:scale-110 hover:bg-slate-950 active:scale-105 '
+        >
+          Submit{" "}
+          <FaPaperPlane className='text-xs opacity-80 transition-all group-hover:translate-x-1 group-hover:-translate-y-1' />
         </button>
       </form>
-    </section>
+    </motion.section>
   );
 }
