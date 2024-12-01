387b6c9888bee7ae10a95485654804fda8bdd137
Harry Dhillon <hdhillon478@gmail.com>
2024-12-01 14:56:12 +0000

    feat: enhance sendEmail action with validation and error handling

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,7 +1,32 @@
 "use server";
 
+import { Resend } from "resend";
+
+import { validateString, getErrorMessage } from "@/lib/utils";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export async function sendEmail(formData: FormData) {
-  console.log("Sending email...");
-  console.log(formData.get("senderEmail"));
-  console.log(formData.get("message"));
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
+
+  if (!validateString(message, 500)) {
+    return { error: "Message is required" };
+  }
+
+  if (!validateString(senderEmail, 5000)) {
+    return { error: "Email is required" };
+  }
+
+  try {
+    await resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: "hdhillon478@gmail.com",
+      subject: "Message from portfolio contact form",
+      replyTo: senderEmail as string,
+      text: message as string,
+    });
+  } catch (error: unknown) {
+    return { error: getErrorMessage(error) };
+  }
 }
