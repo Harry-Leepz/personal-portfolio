fd10e9a2ee49386943ab9d2e8edd2027260c514b
Harry Dhillon <hdhillon478@gmail.com>
2024-12-02 20:33:00 +0000

    feat: enhance LightDarkBtn to manage dark mode class on document element

--- components/buttons/light-dark-btn.tsx
+++ components/buttons/light-dark-btn.tsx
@@ -12,8 +12,12 @@ export default function LightDarkBtn() {
     const localTheme = window.localStorage.getItem("theme") as Theme | null;
     if (localTheme) {
       setTheme(localTheme);
+      if (localTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      }
     } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
       setTheme("dark");
+      document.documentElement.classList.add("dark");
     }
   }, []);
 
@@ -21,9 +25,11 @@ export default function LightDarkBtn() {
     if (theme === "light") {
       setTheme("dark");
       window.localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
     } else {
       setTheme("light");
       window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
