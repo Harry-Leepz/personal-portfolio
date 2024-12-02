80976b020b34dfded3db6c1d3d2873c7dce42766
Harry Dhillon <hdhillon478@gmail.com>
2024-12-02 20:24:35 +0000

    feat: implement theme toggle functionality in LightDarkBtn component

--- components/buttons/light-dark-btn.tsx
+++ components/buttons/light-dark-btn.tsx
@@ -1,13 +1,40 @@
-import { BsSun } from "react-icons/bs";
+"use client";
+
+import { useState, useEffect } from "react";
+import { BsMoon, BsSun } from "react-icons/bs";
+
+type Theme = "light" | "dark";
 
 export default function LightDarkBtn() {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+    if (localTheme) {
+      setTheme(localTheme);
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    }
+  }, []);
+
+  const handleThemeClick = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+    }
+  };
+
   return (
     <button
       className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem]
         border border-white border-opactiy-40 shadow-2xl rounded-full font-2xl flex items-center justify-center
         hover:scale-[1.15] active:scale-105 transition-all'
+      onClick={handleThemeClick}
     >
-      <BsSun />
+      {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
   );
 }
