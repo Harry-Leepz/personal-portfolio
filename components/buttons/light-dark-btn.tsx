afc6c2e7795226304c51c89258fdba806f024b1a
Harry Dhillon <hdhillon478@gmail.com>
2024-12-02 20:10:16 +0000

    feat: add new LightDarkBtn component

---
+++ components/buttons/light-dark-btn.tsx
@@ -0,0 +1,13 @@
+import { BsSun } from "react-icons/bs";
+
+export default function LightDarkBtn() {
+  return (
+    <button
+      className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem]
+        border border-white border-opactiy-40 shadow-2xl rounded-full font-2xl flex items-center justify-center
+        hover:scale-[1.15] active:scale-105 transition-all'
+    >
+      <BsSun />
+    </button>
+  );
+}
