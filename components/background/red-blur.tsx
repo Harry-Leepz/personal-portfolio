b848cd8c5b84ea8b07c52f5d74fcdf6bff31e014
Harry Dhillon <hdhillon478@gmail.com>
2024-12-04 20:07:26 +0000

    feat: add dark mode background colors to BlueBlur and RedBlur components

--- components/background/red-blur.tsx
+++ components/background/red-blur.tsx
@@ -2,7 +2,8 @@ export default function RedBlur() {
   return (
     <div
       className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem]
-    h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'
+    h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]
+    dark:bg-[#946263]'
     ></div>
   );
 }
