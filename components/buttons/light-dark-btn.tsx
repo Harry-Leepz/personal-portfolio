853c46c17236a95c20cbcdd332903aa8b9c91b95
Harry Dhillon <hdhillon478@gmail.com>
2024-12-04 20:08:20 +0000

    style: add dark mode background color to LightDarkBtn component

--- components/buttons/light-dark-btn.tsx
+++ components/buttons/light-dark-btn.tsx
@@ -37,7 +37,7 @@ export default function LightDarkBtn() {
     <button
       className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem]
         border border-white border-opactiy-40 shadow-2xl rounded-full font-2xl flex items-center justify-center
-        hover:scale-[1.15] active:scale-105 transition-all'
+        hover:scale-[1.15] active:scale-105 transition-all dark:bg-slate-950'
       onClick={handleThemeClick}
     >
       {theme === "light" ? <BsSun /> : <BsMoon />}
