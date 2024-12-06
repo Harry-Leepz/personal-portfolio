6159270c7dbdff069ce7044aadb36240b3a3cf19
Harry Dhillon <hdhillon478@gmail.com>
2024-12-06 17:53:41 +0000

    style: update dark mode text color for active section in Header component

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -48,7 +48,7 @@ export default function Header() {
                 className={clsx(
                   "flex w-full items-center justify-center px-3 py-3 hover:text-slate-950 transition dark:text-slate-400 dark:hover:text-slate-300",
                   {
-                    "text-slate-950 dark:text-slate-200":
+                    "text-slate-950 dark:text-slate-300":
                       currentActiveSection === link.name,
                   }
                 )}
