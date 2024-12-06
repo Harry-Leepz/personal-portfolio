44d6680dc3b3171b5a9e56e6946461572e35d3b6
Harry Dhillon <hdhillon478@gmail.com>
2024-12-06 18:06:04 +0000

    style: enhance dark mode support in Project component

--- components/sections/project.tsx
+++ components/sections/project.tsx
@@ -29,20 +29,21 @@ export default function Project({
       ref={ref}
       style={{ scale: scaleProgress, opacity: opacityProgress }}
       className='bg-slate-50 max-w-[42rem] border border-black/5 
-      overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 group even:pl-8 hover:bg-slate-100 transition rounded-lg'
+      overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 group even:pl-8 hover:bg-slate-100 transition rounded-lg
+      dark:bg-white/10 dark:hover:bg-white/20 dark:text-white'
     >
       <div className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]'>
         <a href={link} target='_blank' className='flex items-center gap-2'>
-          <BsGithub className='text-xl dark:text-slate-950' />
-          <h3 className='text-2xl font-semibold dark:text-slate-950'>
-            {title}
-          </h3>
+          <BsGithub className='text-xl dark:text-white' />
+          <h3 className='text-2xl font-semibold dark:text-white'>{title}</h3>
         </a>
-        <p className='mt-2 leading-relaxed text-slate-700'>{description}</p>
+        <p className='mt-2 leading-relaxed text-slate-700 dark:text-white/70'>
+          {description}
+        </p>
         <ul className='flex flex-wrap gap-2 mt-4 sm:mt-auto'>
           {tags.map((tag, index) => (
             <li
-              className='bg-slate-900/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full'
+              className='bg-slate-900/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70'
               key={index}
             >
               {tag}
