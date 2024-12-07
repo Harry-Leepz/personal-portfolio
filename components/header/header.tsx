35a4368977fb44dcffad747094a35d56df209167
Harry Dhillon <hdhillon478@gmail.com>
2024-12-07 06:50:17 +0000

    style: adjust Header component height for improved visual consistency

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -24,7 +24,7 @@ export default function Header() {
   return (
     <header className='z-[999] relative'>
       <motion.div
-        className='fixed top-0 left-1/2 -translate-x-1/2 h-[5.8rem] w-full rounded-none border border-white 
+        className='fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white 
         border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.3] backdrop-blur-[0.5rem] 
         sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-slate-950 dark:border-black/40 dark:bg-opacity-75'
         initial={{ y: -300, x: "-50%", opacity: 0 }}
@@ -33,7 +33,7 @@ export default function Header() {
 
       <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[inital] sm:py-0'>
         <ul
-          className='flex w-[22.rem] flex-wrap items-center justify-center 
+          className='flex w-[22rem] flex-wrap items-center justify-center 
           gap-y-1 text-[0.9rem] font-medium text-slate-500 sm:w-[initial] sm:flex-nowrap sm:gap-5'
         >
           {navLinks.map((link) => (
